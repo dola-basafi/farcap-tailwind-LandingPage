39a8ed7cc0b2275654da878398e3aa852d92b11c
Abdullah  Mochammad Basafi <dola.doit@gmail.com>
2022-11-18 17:46:18 +0700

    progress hero section

---
+++ script.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    fontFamily:{
+      'sans':['Poppins']
+    },
+    // container: {      
+    //   center:true,
+    //   screens: {        
+    //     xl: "1170px",
+    //   },
+    // },
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        myOrange: "#FB9333",
+      },
+    },
+  },
+};
